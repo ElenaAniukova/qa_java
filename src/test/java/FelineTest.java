0f7aacd6a11a22fb72ef5e65997a3fa436ebb5f3
ElenaAniukova <soffia27@mail.ru>
2025-12-22 19:20:07 +0100

    добавлены тесты и обновлены классы

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import com.example.Feline;
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.List;
+
 
 public class FelineTest {
+
+    @Test
+    public void testEatMeat() throws Exception {
+        Feline feline = new Feline();
+        List<String> expectedFood = List.of("Животные", "Птицы", "Рыба");
+        Assert.assertEquals(expectedFood, feline.eatMeat());
+    }
+
+    @Test
+    public void testGetFamily() {
+        Feline feline = new Feline();
+        Assert.assertEquals("Кошачьи", feline.getFamily());
 }
+
+    @Test
+    public void testGetKittens() {
+        Feline feline = new Feline();
+        Assert.assertEquals(1, feline.getKittens());
+    }
+
+    @Test
+    public void testGetKittensWithParameter() {
+        Feline feline = new Feline();
+        Assert.assertEquals(2, feline.getKittens(2));
+    }
+}
+
